66ab8d62cf52d5359364bfddc90894735948106e
Chris <christian.mueller@uni-a.de>
2025-11-14 18:15:03 +0100

    Use template for home page

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hallo von meiner ersten Web-App! 🎓"
+    return render_template("index.html", title="Meine Uni-WebApp")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
